d46e840ef4e08552272044119fcbeac9fa71a693
murn0 <47878863+murn0@users.noreply.github.com>
2023-06-10 02:30:04 +0900

    update.

--- scripts/get-current-version.sh
+++ scripts/get-current-version.sh
@@ -7,4 +7,6 @@ CURRENT_VERSION="$(gh release list --limit 1 --exclude-drafts --exclude-pre-rele
 if [[ -z "${CURRENT_VERSION}" ]]; then
     CURRENT_VERSION="v0.0.0"
 fi
+
+echo $CURRENT_VERSION
 echo "current_version=$CURRENT_VERSION" >> $GITHUB_OUTPUT
